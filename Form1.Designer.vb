76449033946c32f807d9af4a60dbc58fd9ef810c
wong lik <wonglik0817@outlook.com>
2021-04-20 13:01:35 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPapar1 = New System.Windows.Forms.Button()
+        Me.lblResult1 = New System.Windows.Forms.Label()
+        Me.txtBox1 = New System.Windows.Forms.TextBox()
+        Me.lblResult2 = New System.Windows.Forms.Label()
+        Me.btnPapar2 = New System.Windows.Forms.Button()
+        Me.lblResult3 = New System.Windows.Forms.Label()
+        Me.btnPapar3 = New System.Windows.Forms.Button()
+        Me.lblResult4 = New System.Windows.Forms.Label()
+        Me.btnPapar4 = New System.Windows.Forms.Button()
+        Me.txtBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnPapar1
+        '
+        Me.btnPapar1.Location = New System.Drawing.Point(339, 46)
+        Me.btnPapar1.Name = "btnPapar1"
+        Me.btnPapar1.Size = New System.Drawing.Size(75, 23)
+        Me.btnPapar1.TabIndex = 0
+        Me.btnPapar1.Text = "send"
+        Me.btnPapar1.UseVisualStyleBackColor = True
+        '
+        'lblResult1
+        '
+        Me.lblResult1.AutoSize = True
+        Me.lblResult1.Location = New System.Drawing.Point(468, 49)
+        Me.lblResult1.Name = "lblResult1"
+        Me.lblResult1.Size = New System.Drawing.Size(0, 17)
+        Me.lblResult1.TabIndex = 1
+        '
+        'txtBox1
+        '
+        Me.txtBox1.Location = New System.Drawing.Point(208, 113)
+        Me.txtBox1.Name = "txtBox1"
+        Me.txtBox1.Size = New System.Drawing.Size(100, 22)
+        Me.txtBox1.TabIndex = 2
+        '
+        'lblResult2
+        '
+        Me.lblResult2.AutoSize = True
+        Me.lblResult2.Location = New System.Drawing.Point(468, 118)
+        Me.lblResult2.Name = "lblResult2"
+        Me.lblResult2.Size = New System.Drawing.Size(0, 17)
+        Me.lblResult2.TabIndex = 4
+        '
+        'btnPapar2
+        '
+        Me.btnPapar2.Location = New System.Drawing.Point(339, 112)
+        Me.btnPapar2.Name = "btnPapar2"
+        Me.btnPapar2.Size = New System.Drawing.Size(75, 23)
+        Me.btnPapar2.TabIndex = 3
+        Me.btnPapar2.Text = "send"
+        Me.btnPapar2.UseVisualStyleBackColor = True
+        '
+        'lblResult3
+        '
+        Me.lblResult3.AutoSize = True
+        Me.lblResult3.Location = New System.Drawing.Point(468, 199)
+        Me.lblResult3.Name = "lblResult3"
+        Me.lblResult3.Size = New System.Drawing.Size(0, 17)
+        Me.lblResult3.TabIndex = 6
+        '
+        'btnPapar3
+        '
+        Me.btnPapar3.Location = New System.Drawing.Point(339, 193)
+        Me.btnPapar3.Name = "btnPapar3"
+        Me.btnPapar3.Size = New System.Drawing.Size(75, 23)
+        Me.btnPapar3.TabIndex = 5
+        Me.btnPapar3.Text = "send"
+        Me.btnPapar3.UseVisualStyleBackColor = True
+        '
+        'lblResult4
+        '
+        Me.lblResult4.AutoSize = True
+        Me.lblResult4.Location = New System.Drawing.Point(468, 268)
+        Me.lblResult4.Name = "lblResult4"
+        Me.lblResult4.Size = New System.Drawing.Size(0, 17)
+        Me.lblResult4.TabIndex = 9
+        '
+        'btnPapar4
+        '
+        Me.btnPapar4.Location = New System.Drawing.Point(339, 264)
+        Me.btnPapar4.Name = "btnPapar4"
+        Me.btnPapar4.Size = New System.Drawing.Size(75, 23)
+        Me.btnPapar4.TabIndex = 8
+        Me.btnPapar4.Text = "send"
+        Me.btnPapar4.UseVisualStyleBackColor = True
+        '
+        'txtBox2
+        '
+        Me.txtBox2.Location = New System.Drawing.Point(208, 265)
+        Me.txtBox2.Name = "txtBox2"
+        Me.txtBox2.Size = New System.Drawing.Size(100, 22)
+        Me.txtBox2.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(511, 417)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(261, 24)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "WONG LIK 18DDT19F1028"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblResult4)
+        Me.Controls.Add(Me.btnPapar4)
+        Me.Controls.Add(Me.txtBox2)
+        Me.Controls.Add(Me.lblResult3)
+        Me.Controls.Add(Me.btnPapar3)
+        Me.Controls.Add(Me.lblResult2)
+        Me.Controls.Add(Me.btnPapar2)
+        Me.Controls.Add(Me.txtBox1)
+        Me.Controls.Add(Me.lblResult1)
+        Me.Controls.Add(Me.btnPapar1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnPapar1 As Button
+    Friend WithEvents lblResult1 As Label
+    Friend WithEvents txtBox1 As TextBox
+    Friend WithEvents lblResult2 As Label
+    Friend WithEvents btnPapar2 As Button
+    Friend WithEvents lblResult3 As Label
+    Friend WithEvents btnPapar3 As Button
+    Friend WithEvents lblResult4 As Label
+    Friend WithEvents btnPapar4 As Button
+    Friend WithEvents txtBox2 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+
+    Private Sub S1()
+        lblResult1.Text = "DDT4C"
+    End Sub
+
+    Private Sub btnPapar1_Click(sender As Object, e As EventArgs) Handles btnPapar1.Click
+        S1()
+    End Sub
+
+    Private Sub S2()
+        Dim strText As String
+        strText = txtBox1.Text
+        lblResult2.Text = strText
+    End Sub
+
+    Private Sub btnPapar2_Click(sender As Object, e As EventArgs) Handles btnPapar2.Click
+        S2()
+    End Sub
+
+    Private Function F1()
+        Return "Hello World"
+    End Function
+
+    Private Sub btnPapar3_Click(sender As Object, e As EventArgs) Handles btnPapar3.Click
+        lblResult3.Text = F1()
+    End Sub
+
+    Private Function F2()
+        Dim strText2 As String
+        strText2 = txtBox2.Text
+        Return strText2
+    End Function
+
+    Private Sub btnPapar4_Click(sender As Object, e As EventArgs) Handles btnPapar4.Click
+        lblResult4.Text = F2()
+    End Sub
+End Class
